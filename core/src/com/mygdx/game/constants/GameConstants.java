422395cbcdb2bf4adb3065fedf5a29b760e7df75
Frederico Alexandre <frederico.alexandre94@hotmail.com>
2017-09-18 18:34:40 +0200

    Update GameConstants.java

--- core/src/com/mygdx/game/constants/GameConstants.java
+++ core/src/com/mygdx/game/constants/GameConstants.java
@@ -5,18 +5,16 @@ package com.mygdx.game.constants;
  */
 
 public class GameConstants {
-
     public static final String MAIN_VIEW_BACKGROUND = "backgorund_images/mainViewBackgorund.png";
     public static final String TOUCHPAD_BACKGROUND_IMAGE = "backgorund_images/touchBackground.png";
     public static final String TOUCHPAD_KNOB_BACKGROUND_IMAGE = "backgorund_images/touchKnob.png";
     public static final String TOUCHPAD_DIRECTION_BACKGROUND_IMAGE ="backgorund_images/directionTouchBackground.png";
     public static final String TOUCHPAD_DIRECTION__KNOB_BACKGROUND_IMAGE ="backgorund_images/directionTouchKnob.png";
-
+    
     public static final String TOUCHPAD_BACKGROUND = "touchBackground";
     public static final String TOUCHPAD_KNOB_BACKGROUND = "touchKnob";
     public static final String TOUCHPAD_DIRECTION__BACKGROUND ="directionTouchBackground";
     public static final String TOUCHPAD_DIRECTION__KNOB_BACKGROUND ="directionTouchKnob";
-
 
     public static final int INT_ZERO = 0;
     public static final int INT_TWO = 2;
@@ -24,5 +22,5 @@ public class GameConstants {
     
     public static final float MOVEMENT_TOUCHPAD_DEADZONE_RADIUS = 150.0;
     public static final float DIRECTION_KNOB_DEADZONE_RADIUS = 0.0;
-
+    public static final float CHARACTER_VELOCITY = 5.0;
 }
